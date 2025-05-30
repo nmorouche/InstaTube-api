6b84dcc87e38372e8e9586c1fabb07a55fbb9aa8
Nass <45393026+nmorouche@users.noreply.github.com>
2025-05-30 06:21:56 +0200

    Prepared all the prerequisites for the API to work properly (#1)

---
+++ config.js
@@ -0,0 +1,14 @@
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/instatube';
+const PORT = process.env.PORT || 3000;
+const dbName = process.env.DBNAME || 'instatube';
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+module.exports = {
+    ObjectId,
+    MongoClient,
+    MONGODB_URI,
+    dbName,
+    PORT,
+};
